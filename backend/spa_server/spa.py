62806fb29f7c1f9ba6ed9b5df294a323f6be8082
A Uribe <auribe9@ucmerced.edu>
2021-02-22 18:54:06 -0800

    updated front and backend code to get the sun's elevation and azimuth angle for a point clicked

---
+++ backend/spa_server/spa.py
@@ -0,0 +1,50 @@
+from flask import Flask
+from flask import request
+from flask import render_template
+from flask import jsonify
+from flask import make_response
+import json
+import pandas as pd
+import pvlib
+import requests
+
+app = Flask(__name__)
+
+API_KEY = '6e63c47a00540c88d1241ecb9fd1be17'
+
+OPEN_WEATHER_CURRENT = 'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&units=metric&appid={API_key}'
+
+'''
+@app.route('/weather/<lat>/<lon>', methods=["GET"])
+def get_pressure_and_temperature(lat, lon):
+    response = requests.get(OPEN_WEATHER_CURRENT.format(lat=lat, lon=lon, API_key=API_KEY))
+    if(response.ok):
+        weatherJson = json.loads(response.text)
+        #print(weatherJson)
+        temp = weatherJson['main']['temp']
+        pressure = weatherJson['main']['pressure']
+        print(temp, pressure)
+        return make_response(jsonify({"temp" : str(temp), "pressure": str(pressure)}), 200)
+    return make_response(jsonify({"message": "Request was bad"}), 400)
+'''
+# TODO:: fix case when error message is received
+def _get_pressure_and_temperature(lat, lon):
+    response = requests.get(OPEN_WEATHER_CURRENT.format(lat=lat, lon=lon, API_key=API_KEY))
+    if(response.ok):
+        weatherJson = json.loads(response.text)
+        #print(weatherJson)
+        temp = weatherJson['main']['temp']
+        pressure = weatherJson['main']['pressure']
+        print(temp, pressure)
+        return {"temp" : temp, "pressure": pressure}
+    return make_response(jsonify({"message": "Request was bad"}), 400)
+# Definelty need a better way to route all this data but this should allow js app to get sun's position
+@app.route('/spa/<date>/<time>/<lat>/<lon>/<elev>')
+def get_sun_position(date, time, lat, lon, elev):
+    weather = _get_pressure_and_temperature(lat, lon)
+    sol = pvlib.solarposition.get_solarposition(time=pd.DatetimeIndex([str(date) + ' ' + str(time)]),
+        latitude=float(lat), longitude=float(lon), altitude=float(elev), pressure=float(weather['pressure']) * 100, temperature=float(weather['temp']))
+    print('\nCHECKING IF THIS PRINTS\n',sol,'\n')
+    return make_response(jsonify({'elevation': str(sol['elevation'][0]) ,'azimuth': str(sol['azimuth'][0])}), 200)
+
+app.run(host='127.0.0.1', port=3000)
---
+++ webApp/index.js
@@ -0,0 +1,48 @@
+let map;
+let lat = 36.67998425155742;
+let lon = -121.66129198121926;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: {lat: lat, lng: lon},
+        zoom: 8
+    });
+
+    const elevator = new google.maps.ElevationService();
+    //const infowindow = new google.maps.InfoWindow({});
+    map.addListener("click", (clickEvent) =>{
+        latLngJSON = clickEvent.latLng.toJSON();
+        lat = latLngJSON.lat;
+        lon = latLngJSON.lng;
+
+        elevator.getElevationForLocations(
+            {
+                locations: [clickEvent.latLng]
+            },
+            (results, status) => {
+                if(status == "OK" && results){
+                    console.log('lat: ' + lat + ' lon: ' + lon + ' elevation: ' + results[0].elevation + 'm');
+                    let currentDateObj = new Date(Date.now());
+                    let date = currentDateObj.getUTCFullYear().toString() + '-' + currentDateObj.getUTCMonth().toString() + '-' + currentDateObj.getUTCDate().toString(); 
+                    let time = currentDateObj.getUTCHours().toString() + ':' + currentDateObj.getUTCMinutes().toString() + ':' + currentDateObj.getUTCSeconds().toString();
+                    //TODO:: modify call to get sun position instead
+                    let spa_url = 'http://localhost:3000/spa/' + date + '/' + time + '/' + lat.toString() + '/' + lon.toString() + '/' + results[0].elevation.toString();
+                    let spaHttp = new XMLHttpRequest();
+                    spaHttp.open("Get", spa_url, false);
+                    spaHttp.send();
+                    console.log("response of weather: " + spaHttp.responseText);
+                    
+                    //url = 'localhost:4000/spa/' + date + '/' + time + '/' + lat + '/' + lon + '/' + results[0].elevation + 
+                }
+                else if (status == "INVALID_REQUEST"){
+                    console.log("Invalid request");
+                }
+                else{
+                    console.log("other issue with request");
+                }
+            }
+            
+        );
+        
+    });
+}
